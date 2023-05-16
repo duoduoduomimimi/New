984821b88456602db9c9ccd32f2f92e95c756fbb
duoduoduomimimi <duoduoduomimimi@gmail.com>
2023-05-16 20:45:40 +0800

    daily c code exercises in 5.16

---
+++ 05_16/New.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	//float a = 3.0 / 2;
+//	//printf("%.2f\n", a);
+//
+//	int a = 9 % 2; // 9/2 .....1 得到的是余数 0 ~ 1
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+int main()
+{
+	//int a = 5;
+	//int b = a << 1; // a向左移动一位
+	//int c = a >> 1; // a向右移动一位
+	//printf("%d\n", a); // 101 - 2
+	//printf("%d\n", b); // 1010 - 10
+	//printf("%d\n", c); // 10  - 5
+
+	int a = -5;
+	int b = a << 1;
+	int c = a >> 1;
+	printf("%d\n", a);
+	printf("%d\n", b);
+	printf("%d\n", c);
+
+
+	return 0;
+
+}
