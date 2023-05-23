07ee6b4b0e598eb0b958c1bd57f93f192a7e77a8
duoduoduomimimi <duoduoduomimimi@gmail.com>
2023-05-23 20:52:18 +0800

    daily c code exercises in 5.23

---
+++ 05_23/New.c
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 0;
+//	int* pa = &a; // 一级指针
+//	int** ppa = &pa; // 二级指针
+//	int*** pppa = &ppa; // 三级指针
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	int c = 3;
+//	int* arr[3] = { &a, &b, &c }; // 指针数组
+//	for (int i = 0; i < 3; i++)
+//	{
+//		printf("%d: %p\n", *arr[i], arr[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int data1[] = { 1, 2, 3, 4, 5 };
+//	int data2[] = { 2, 3, 4, 5, 6 };
+//	int data3[] = { 3, 4, 5, 6, 7 };
+//	int* arr[3] = { data1, data2, data3 };
+//	for (int i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 5; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//typedef struct Stu
+//{
+//	char name[10];
+//	int age;
+//	char sex[5];
+//	float score;
+//}Stu;
+//
+//int main()
+//{
+//	struct Stu s1;
+//	Stu s2;
+//
+//	return 0;
+//}
+
+//typedef struct Point
+//{
+//	int x;
+//	int y;
+//}Point;
+//
+//int main()
+//{
+//	Point p1 = { 1,2 };
+//	return 0;
+//}
+
