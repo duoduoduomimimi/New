298868045fdb9daa5b1b2fad0ee744bc58763574
duoduoduomimimi <duoduoduomimimi@gmail.com>
2023-06-12 21:15:00 +0800

    daily c code exercises in 6.12

---
+++ 06_12/New.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//typedef struct Book
+//{
+//	char name[20]; // 书名
+//	float price; // 价格
+//	char id[10]; // 编号
+//}Book;
+//
+//int main()
+//{
+//	struct Book n1 = { "《高质量C/C++编程》", 54.5f,"B1002" };
+//	printf("%-10s %.2f %-8s\n", n1.name, n1.price, n1.id);
+//	Book n2 = { "《C指针与陷阱》",66.7f, "B1006" };
+//	Book* pn = &n2;
+//	printf("%-10s %.2f %-8s\n", pn->name, pn->price, pn->id);
+//	return 0;
+//}
+
+//struct S3
+//{
+//	double d;
+//	char c;
+//	int i;
+//};
+//
+//struct S4
+//{
+//	char c1;
+//	struct S3 s3;
+//	double d;
+//};
+//
+//int main()
+//{
+//
+//	printf("%d\n", sizeof(struct S4));
+//	return 0;
+//}
+
